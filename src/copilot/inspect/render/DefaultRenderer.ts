fe5f120094b0070d2e41f2266acfe14cc7d02e8f
wangmi <wangmi@microsoft.com>
2024-04-19 22:38:23 +0800

    allow user to configure inspection display types via settings.

--- src/copilot/inspect/render/DefaultRenderer.ts
+++ src/copilot/inspect/render/DefaultRenderer.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { ExtensionContext, TextDocument } from "vscode";
+import { ExtensionContext, TextDocument, WorkspaceConfiguration, workspace } from "vscode";
 import { CodeLensRenderer } from "./CodeLensRenderer";
 import { DiagnosticRenderer } from "./DiagnosticRenderer";
 import { GutterIconRenderer } from "./GutterIconRenderer";
 import { RulerHighlightRenderer } from "./RulerHighlightRenderer";
 import { Inspection } from "../Inspection";
 import { InspectionRenderer } from "./InspectionRenderer";
+import { sendInfo } from "vscode-extension-telemetry-wrapper";
 
 export class DefaultRenderer implements InspectionRenderer {
     private readonly renderers: { [type: string]: InspectionRenderer } = {};
@@ -16,7 +17,39 @@ export class DefaultRenderer implements InspectionRenderer {
         this.renderers['guttericons'] = new GutterIconRenderer();
         this.renderers['codelenses'] = new CodeLensRenderer();
         this.renderers['rulerhighlights'] = new RulerHighlightRenderer();
-        this.installedRenderers = Object.values(this.renderers);
+        workspace.onDidChangeConfiguration(event => {
+            if (event.affectsConfiguration('java.copilot.inspection.renderer')) {
+                const settings = this.reinstallRenderers();
+                sendInfo('java.copilot.inspection.renderer.changed', { 'settings': `${settings.join(',')}` });
+            }
+        });
+        this.reinstallRenderers();
+    }
+
+    private reinstallRenderers(): string[] {
+        this.installedRenderers.splice(0, this.installedRenderers.length);
+        const settings = this.reloadSettings();
+        Object.entries(this.renderers).forEach(([type, renderer]) => {
+            if (settings.includes(type.toLowerCase())) {
+                this.installedRenderers.push(renderer);
+                renderer.install(this.context);
+            } else {
+                renderer.uninstall();
+            }
+        });
+        return settings;
+    }
+
+    private reloadSettings(): string[] {
+        const config: WorkspaceConfiguration = workspace.getConfiguration('java.copilot.inspection.renderer');
+        const types: string[] = Object.keys(this.renderers);
+        const settings = types.map(type => config.get<boolean>(type) ? type.toLowerCase() : '').filter(t => t);
+        if (settings.length === 0) {
+            settings.push('diagnostics');
+            settings.push('codelenses');
+            settings.push('rulerhighlights');
+        }
+        return settings;
     }
 
     public install(context: ExtensionContext): void {
